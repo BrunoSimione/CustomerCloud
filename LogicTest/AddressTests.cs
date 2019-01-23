fee4565240f5ab9589734493031bf9c8f62b46a9
John Hinz <jhinz402@hotmail.com>
2019-01-23 16:16:14 -0500

    Updated address integration tests

--- LogicTest/AddressTests.cs
+++ LogicTest/AddressTests.cs
@@ -9,30 +9,40 @@ namespace LogicTest
     [TestClass]
     public class AddressTests
     {
-        private AddressLogic _address;
+        private AddressLogic _addressLogic;
+        private AddressDTO _addressLogicDTO;
 
         [TestInitialize]
         public void Init()
         {
-            _address = new AddressLogic();
+            _addressLogic = new AddressLogic();
+            _addressLogicDTO = new AddressDTO()
+                {
+                    Id = Guid.NewGuid(),
+                    City = Faker.Address.USCity(),
+                    Street = Faker.Address.StreetName(),
+                    Number = Faker.Number.RandomNumber(),
+                    PostalCode = Faker.Address.CanadianZipCode(),
+                    Province = Faker.Address.Province()
+                };
         }
 
         [TestMethod]
         public void Create()
         {
-            Guid id = Guid.NewGuid();
-            _address.Create(new AddressDTO()
-            {
-                Id = id,
-                City = Faker.Address.USCity(),
-                Street = Faker.Address.StreetName(),
-                Number = Faker.Number.RandomNumber(),
-                PostalCode = Faker.Address.CanadianZipCode(),
-                Province = Faker.Address.Province()
-            });
+            _addressLogic.Create(_addressLogicDTO);
+            AddressDTO result = _addressLogic.Read(_addressLogicDTO.Id);
 
-            AddressDTO result = _address.Read(id);
             Assert.IsNotNull(result);
         }
+
+        public void Update()
+        {
+            
+           // _addressLogic.Update( );
+        }
+
+        
+
     }
 }
