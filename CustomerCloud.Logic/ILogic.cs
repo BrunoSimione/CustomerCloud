21bce9d330528bbc4872f54a19b38608feb48468
John Hinz <jhinz402@hotmail.com>
2019-01-23 14:51:57 -0500

    Implemeted the repository pattern

---
+++ CustomerCloud.Logic/ILogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerCloud.Logic
+{
+    interface ILogic<T>
+    {
+        void Create(T item);
+        T Read(T item);
+        void Update(T item);
+        void Delete(T item);
+    }
+}
---
+++ CustomerCloud.Repository/Repository.cs
@@ -0,0 +1,45 @@
+﻿using CustomerCloud.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerCloud.Repository
+{
+    public class Repository<T> where T : class, IEntity
+    {
+        private CustomerContext _context;
+
+        public Repository()
+        {
+            _context = new CustomerContext();
+        }
+
+        public void Create(T item)
+        {
+            _context.Entry<T>(item).State = EntityState.Added;
+            _context.SaveChangesAsync();
+        }
+
+        public T Read(Guid Id)
+        {
+            IQueryable<T> dbQuery = _context.Set<T>();
+            return dbQuery.FirstOrDefault(i => i.Id == Id);
+        }
+
+        public void Update(T item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChangesAsync();
+        }
+
+        public void Delete(T item)
+        {
+            _context.Entry(item).State = EntityState.Deleted;
+            _context.SaveChangesAsync();
+        }
+
+    }
+}
